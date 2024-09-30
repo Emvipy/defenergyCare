62774dcf3836547c736d461997025fc0a701f008
Emvipy <152436727+Emvipy@users.noreply.github.com>
2024-09-30 09:15:49 -0600

    1.0.30F
    1.0.30 new

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+import OneSignalFramework
+
 import Flutter
 
 @UIApplicationMain
@@ -9,6 +11,17 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      OneSignal.Debug.setLogLevel(.LL_VERBOSE)
+        
+        // OneSignal initialization
+        OneSignal.initialize("YOUR_ONESIGNAL_APP_ID", withLaunchOptions: launchOptions)
+        
+        // requestPermission will show the native iOS notification permission prompt.
+        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+        OneSignal.Notifications.requestPermission({ accepted in
+          print("User accepted notifications: \(accepted)")
+        }, fallbackToSettings: true)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
